618c2f24feee974048633dba44bcf86465dad0c4
Bia <fabianazs@gmail.com>
2019-05-02 22:43:16 -0300

    Atualizando node.js

--- nodejs/PrimeiroProjeto.js
+++ nodejs/PrimeiroProjeto.js
@@ -10,14 +10,13 @@
 
 //DESAFIO contar tempo inicial, contar tempo final - imprimir tempo gasto
 var dataInicial = new Date();
-var tempo;
-for(let i=1;i<10000;i++){
-    tempo = new Date();
+
+for(let i=1;i<1000000;i++){
+    var j = i++;
 }
+var tempo = new Date();
 var tempoFinal = parseInt(tempo.getMilliseconds());  
 var tempoInicial = parseInt(dataInicial.getMilliseconds());
 
 var resultado = tempoFinal - tempoInicial;
-//  console.log(tempoInicial.getSeconds());
-//  console.log(tempoFinal.getSeconds());
 console.log('O tempo total gasto foi: '+resultado+' milisegundos');
